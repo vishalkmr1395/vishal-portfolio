0f9f2beade5e8c9dacf2a1fe7e06b491ad755d4d
Vishal Kumar <vishalkmr1395@gmail.com>
2026-02-17 10:56:25 -0500

    Update resume link in HeroComponent

--- src/components/HeroComponent/Hero.jsx
+++ src/components/HeroComponent/Hero.jsx
@@ -52,7 +52,7 @@ export default function Hero() {
         <Eye size={20} />
       </a>
       <a
-        href="src/assets/sibghatullah-khan-resume.pdf"
+        href="src/assets/Vishal Kumar Subramanian - TPM.pdf"
         target="_blank"
         download="Vishal_Kumar_Resume.pdf"
         rel="noopener noreferrer"
